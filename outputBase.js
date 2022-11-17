60babeeaeef11667dfe4137564922a813eed67d7
starydv7 <pawanstar72926@gmail.com>
2022-11-17 19:23:27 +0530

    first commit

---
+++ outputBase.js
@@ -0,0 +1,11 @@
+//check string and number
+let a = [20];
+let b = ["20"];
+console.log(a[0] == b[0]);
+console.log(a[0] === b[0]);
+
+//check object
+
+let obj1 = [];
+let obj2 = [];
+console.log(obj1 === obj2);
