a0419272749397d999aac4f997e0dca544ecf53e
starydv7 <pawanstar72926@gmail.com>
2022-11-18 12:10:41 +0530

    typeof

---
+++ timeconverter.js
@@ -0,0 +1,21 @@
+function timeConversion(s) {
+    // Write your code here
+    const [hour, minutes, secondsWithCharacters] = s.split(':');
+  const characters = secondsWithCharacters.slice(-2).toLowerCase();
+  const seconds = secondsWithCharacters.slice(0, 2);
+  let twelveHours = 12;
+  let convertedTime = '';
+
+  if (characters === 'pm') {
+    convertedTime = Number(hour == 12 ? '00' : hour) + twelveHours;
+  }
+  if (characters === 'am') {
+    convertedTime = hour == 12 ? '00' : hour;
+  }
+
+  return `${convertedTime}:${minutes}:${seconds}`;
+
+}
+var s = '12:51:61PM';
+const res = timeConversion(s);
+console.log(res);
