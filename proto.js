ddc6fe94622833335108fd820cc1769b76d71d4a
starydv7 <pawanstar72926@gmail.com>
2022-11-19 15:10:49 +0530

    proto

--- proto.js
+++ proto.js
@@ -8,3 +8,11 @@ const musician = {
 Object.setPrototypeOf(musician, person);
 console.log(musician.plays);
 console.log(musician.alive);
+
+const guitarist = {
+    strings: 6,
+    __proto__:musician
+}
+console.log(guitarist.alive);
+console.log(guitarist.plays);
+console.log(guitarist.strings);
