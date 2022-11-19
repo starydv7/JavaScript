ab62727317dd3ef00578599651938df4951adc76
starydv7 <pawanstar72926@gmail.com>
2022-11-19 21:58:28 +0530

    proto

--- outputBase.js
+++ outputBase.js
@@ -91,3 +91,38 @@ var arr2 = [5, 444, 4, 3, 2, 2, 1, 6, 6, 7, 8, 9, 10];
 let finalArr =[...arr1,...arr2];
 console.log(...arr1, ...arr2);
 console.log(finalArr);
+
+console.log(null == undefined);
+var x = 9;
+var y2 = "6";
+console.log(x - y2)
+function demo() {
+    let dept = "EDTA"
+    console.log(dept);
+}
+let dept = "FASDM";
+demo();
+console.log(dept);
+var object = {
+    name: "Aditya",
+    getName: function () {
+        console.log(this.name);
+    }
+
+}
+var getName = object.getName;
+var object2 = { name: "Not Aditya", getName };
+object2.getName();
+
+let x2 = [1, 2, 3];
+var y2 = [4, 5, 6];
+const combinedArray1 = [...x2, ...y2];
+console.log(combinedArray1);
+//b
+let z = [3, 4, 5, 6];
+const combinedArray2 = [1, 2, ...z];
+console.log(combinedArray2);
+//c
+let c = [3, 4, 5, 6];
+const combinedArray3 = [1, 2, ...a];
+console.log(a);
